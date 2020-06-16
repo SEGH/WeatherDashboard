7fb6993573b64835449c7619fc8360c46e0a9f16
Susan Holland <segh@fastmail.com>
2020-06-16 17:19:18 -0400

    Wrote html structure, added cdn links, JS pseudocode

---
+++ script.js
@@ -0,0 +1,17 @@
+// Assignments
+
+// Array of cities prepopulated with a few cities
+
+// Function to display prepopulated city array at page load
+
+// Event listener for input button
+
+// Function to push setItem city name to city array and save in localStorage
+
+// Function to get city array from localStorage and display
+
+// ajax method to getWeather of city at the end of the array
+
+    // Display city name, current date, icon, temperature, humidity, wind speed, UV index
+
+    // Display future weather cards includes date, icon, temperature, and humidity
