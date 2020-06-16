622faefbcb5925cf2a5593954b54ea771685dc38
Susan Holland <segh@fastmail.com>
2020-06-16 19:23:39 -0400

    getWeather function with ajax

--- script.js
+++ script.js
@@ -15,8 +15,8 @@ $("#searchBtn").on("click", function () {
     localStorage.setItem("cities", JSON.stringify(cityArray));
     $("#cityInput").val("");
     renderList();
-
     // displayLast function
+    getWeather(cityArray[cityArray.length - 1]);
 });
 
 // Function to get city array from localStorage and display
@@ -30,11 +30,75 @@ function renderList() {
     })
 }
 // ajax method to getWeather
+function getWeather(city) {
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=1857cc69aec95fbf7e8fded813ff9c45";
 
-    // Display city name, current date, icon, temperature, humidity, wind speed, UV index
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (weatherData) {
+        // Display city name, current date, icon, temperature, humidity, wind speed, UV index
+        console.log(weatherData);
+        var city = weatherData.name;
+        $("#cityName").text(city);
 
-    // Display future weather cards includes date, icon, temperature, and humidity
+        var unixDate = weatherData.dt;
+        var date = moment.unix(unixDate).format("MM/DD/YYYY");
+        $("#jumboDate").text(date);
 
+        var temp = weatherData.main.temp;
+        $("#jumboTemp").html(temp + "&deg;");
+
+        var humidity = weatherData.main.humidity + "%";
+        $("#jumboHumid").text(humidity);
+
+        var wind = weatherData.wind.speed + " MPH";
+        $("#jumboWind").text(wind);
+
+        var icon = weatherData.weather[0].icon;
+        var iconURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+        $("#currentImage").attr("src", iconURL);
+
+        var lat = weatherData.coord.lat;
+        var lon = weatherData.coord.lon;
+
+        var dayQueryURL = "https://api.openweathermap.org/data/2.5/onecall?lat="+ lat + "&lon=" + lon + "&units=imperial&appid=1857cc69aec95fbf7e8fded813ff9c45";
+
+        $.ajax({
+            url: dayQueryURL,
+            method: "GET"
+        }).then(function(dayData) {
+            // Display future weather cards includes date, icon, temperature, and humidity
+            console.log(dayData.daily);
+
+            var uvIndex = dayData.current.uvi;
+            $("#jumboUV").text(uvIndex);
+            if (uvIndex > 8) {
+                $("#jumboUV").addClass("bg-danger");
+            }
+            var cards = $(".card-deck").children();
+            cards.each(function(cardIndex) {
+                console.log(cardIndex);
+                var dataIndex = cardIndex + 1;
+                var cardUnix = dayData.daily[dataIndex].dt;
+                var cardDate = moment.unix(cardUnix).format("MM/DD/YYYY");
+                $(this).find("h5").text(cardDate);
+
+                var cardIcon = dayData.daily[dataIndex].weather[0].icon;
+                var cardIconURL = "http://openweathermap.org/img/wn/" + cardIcon + "@2x.png";
+                $(this).find("img").attr("src", cardIconURL);
+
+                var cardTemp = dayData.daily[dataIndex].temp.day;
+                $(this).find(".cardTemp").html(cardTemp + "&deg;");
+
+                var cardHumid = dayData.daily[dataIndex].humidity + "%";
+                $(this).find(".cardHum").text(cardHumid);
+                
+            });
+        });
+    });
+}
 // Function to getWeather for city at the end of the array
+getWeather(cityArray[cityArray.length - 1]);
 
 // Event listener to getWeather for clicked city
