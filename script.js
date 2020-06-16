ead1a14a599dead33c332ed562e650a4f02ea113
Susan Holland <segh@fastmail.com>
2020-06-16 18:04:40 -0400

    Input, list, and local storage functional

--- script.js
+++ script.js
@@ -1,17 +1,40 @@
 // Assignments
+var list = $("ul");
 
 // Array of cities prepopulated with a few cities
+var cityArray = ["Philadelphia", "Los Angeles", "Tokyo", "Johannesburg"];
 
 // Function to display prepopulated city array at page load
+renderList();
 
-// Event listener for input button
+// Event listener for input button and Function to push setItem city name to city array and save in localStorage
+$("#searchBtn").on("click", function () {
+    var cityInput = $("#cityInput").val().trim();
+    // Add if statement to check whether city is already in array
+    cityArray.push(cityInput);
+    localStorage.setItem("cities", JSON.stringify(cityArray));
+    $("#cityInput").val("");
+    renderList();
 
-// Function to push setItem city name to city array and save in localStorage
+    // displayLast function
+});
 
 // Function to get city array from localStorage and display
-
-// ajax method to getWeather of city at the end of the array
+function renderList() {
+    list.empty();
+    var tempArray = JSON.parse(localStorage.getItem("cities"));
+    cityArray = tempArray;
+    cityArray.forEach(function (city) {
+        var listItem = $("<li>").text(city).addClass("list-group-item");
+        list.prepend(listItem);
+    })
+}
+// ajax method to getWeather
 
     // Display city name, current date, icon, temperature, humidity, wind speed, UV index
 
     // Display future weather cards includes date, icon, temperature, and humidity
+
+// Function to getWeather for city at the end of the array
+
+// Event listener to getWeather for clicked city
